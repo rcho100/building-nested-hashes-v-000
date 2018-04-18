efb0d49ec07427286320581cec059c5d61627191
Richard Cho <richardcho100@gmail.com>
2018-04-18 15:44:04 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,29 +4,26 @@ def third_challenge
       :patriarch => {
         :name => "Lord Montague",
         :age => "53"
-        },
+      },
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
-        },
+      },
       :hero => {
         :name => "Romeo",
         :age => "15",
         :status => "alive"
-        },
+      },
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
-        :name
-        },
-      :matriarch => {
-        :name
-        },
-      :heroine => {
-        :name
-        },
-      :heroine_friends => []
+     :patriarch => {
+       name: "Lord Capulet", 
+       age: "50"
+     },
+     :matriarch => {name: "Lady Capulet", age: "51"},
+     :heroine => {name: "Juliet", age: "15", status: "alive"},
+     :heroine_friends => []
    }
   }
 
